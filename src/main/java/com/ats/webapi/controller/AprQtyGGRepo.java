5e8c48fba5316dc6ae1a35097e9b1f32d4c61ce8
ubuntu <ubuntu@ubuntu-Lenovo-ideapad-330-15IKB>
2021-05-14 11:30:12 +0530

    aprGrn-sac

--- src/main/java/com/ats/webapi/controller/AprQtyGGRepo.java
+++ src/main/java/com/ats/webapi/controller/AprQtyGGRepo.java
@@ -26,7 +26,7 @@ public interface AprQtyGGRepo extends JpaRepository<AprQtyGG, Integer>{
 	 */
     @Query(value = " SELECT t1.*,IFNULL(t2.all_apr_qty,0) as all_apr_qty " + 
     		"    FROM (SELECT t_bill_detail.bill_detail_no,t_bill_detail.bill_qty FROM t_bill_detail WHERE t_bill_detail.bill_detail_no In (:billDetailNoList) ) t1\n" + 
-    		"    LEFT JOIN (SELECT IFNULL(SUM(gg.apr_qty_acc),0) as all_apr_qty,gg.bill_detail_no FROM t_grn_gvn gg WHERE gg.grn_gvn_status=6 and gg.bill_detail_no In(12249) and gg.grn_gvn_id NOT IN (:grnGvnIdList) group by gg.bill_detail_no) t2 on t1.bill_detail_no=t2.bill_detail_no\n" + 
+    		"    LEFT JOIN (SELECT IFNULL(SUM(gg.apr_qty_acc),0) as all_apr_qty,gg.bill_detail_no FROM t_grn_gvn gg WHERE gg.grn_gvn_status=6 and gg.bill_detail_no In (:billDetailNoList) and gg.grn_gvn_id NOT IN (:grnGvnIdList) group by gg.bill_detail_no) t2 on t1.bill_detail_no=t2.bill_detail_no\n" + 
     		"", nativeQuery = true)
 	List<AprQtyGG> getAprQtyGG(@Param("billDetailNoList") List<Integer> billDetailNoList,
 			@Param("grnGvnIdList") List<Integer> grnGvnIdList);
